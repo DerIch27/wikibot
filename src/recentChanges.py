e5fba77d5ba4afd2c93e7f6a3d8afe9e40763f44
STREBER24 <largot@gmx.de>
2025-03-24 10:50:31 +0000

    enhance stream lag handling

--- src/recentChanges.py
+++ src/recentChanges.py
@@ -6,39 +6,19 @@ import telegram
 import requests
 import logging
 import katdisk
+import utils
 import time
 import re
 
-DELAY_THRESHOLD = 300 # 5min
-class LagMonitor:
-    def __init__(self):
-        self.lastLagNotification: float = 0
-        self.numberOfDelayedChanges = 0
-        self.maxLag = 0
-        self.minLag = 9999999999999999
-        self.firstLag: float = 0
-    def formatSeconds(self, timediff: int):
-        result = f'{timediff%60} s'; timediff = timediff // 60
-        if timediff == 0: return result
-        result = f'{timediff%60} min, {result}'; timediff = timediff // 60
-        if timediff == 0: return result
-        result = f'{timediff%24} h, {result}'; timediff = timediff // 24
-        if timediff == 0: return result
-        return f'{timediff} d, {result}'
-    def checkRevision(self, change: dict):
-        lag = int(time.time() - change['timestamp'])
-        if lag > DELAY_THRESHOLD and self.maxLag <= DELAY_THRESHOLD: self.firstLag = time.time()
-        if lag > self.maxLag: self.maxLag = lag
-        if lag < self.minLag: self.minLag = lag
-        if lag > DELAY_THRESHOLD:
-            logging.warning(f'Handled revision {change.get('revision')} with delay of {lag}s')
-            self.numberOfDelayedChanges += 1
-        if (self.maxLag > DELAY_THRESHOLD) and (time.time() - self.lastLagNotification > 600) and (time.time() - self.firstLag > 300):
-            telegram.send(f'LAG WARNING\nminimum: {self.formatSeconds(self.minLag)}\nmaximum: {self.formatSeconds(self.maxLag)}\n{self.numberOfDelayedChanges} delayed changes', silent=True)
-            self.numberOfDelayedChanges = 0
-            self.lastLagNotification = time.time()
-            self.maxLag = 0
-            self.minLag = 9999999999999999
+def formatSeconds(timediff: int):
+    timediff = int(timediff)
+    result = f'{timediff%60} s'; timediff = timediff // 60
+    if timediff == 0: return result
+    result = f'{timediff%60} min, {result}'; timediff = timediff // 60
+    if timediff == 0: return result
+    result = f'{timediff%24} h, {result}'; timediff = timediff // 24
+    if timediff == 0: return result
+    return f'{timediff} d, {result}'
 
 
 def getPageFromRevision(site, revision: int):
@@ -48,17 +28,27 @@ def getPageFromRevision(site, revision: int):
     return data['query']['pages'][pageId]['title']
 
 
+def getStream():
+    stream = eventstreams.EventStreams(streams='recentchange')
+    stream.register_filter(type='edit', wiki='dewiki')
+    return stream
+
+
 def monitorRecentChanges():
     site = pywikibot.Site('de', 'wikipedia')
-    stream = eventstreams.EventStreams(streams='recentchange')
     site.login()
-    stream.register_filter(type='edit', wiki='dewiki')
-    lagMonitor = LagMonitor()
+    stream = getStream()
     while True:
         try:
             change = next(stream)
             logging.debug(f'handle recent change {change.get('revision')} on {change.get('title')}')
-            lagMonitor.checkRevision(change)
+            lag = int(time.time() - change['timestamp'])
+            if lag > 600: # 10min
+                logging.warn(f'received revision {change['revision']['new']} with lag {lag} and reset stream')
+                stream = getStream()
+                lastLagNotification = utils.checkLastUpdate('recent-lagged-stream', 30)
+                if not lastLagNotification[0]:
+                    telegram.send(f'reset stream after lag of {formatSeconds(lag)} ({telegram.difflink(change)}); Last lag {lastLagNotification[1]//60}min ago', silent=True)
             telegram.alarmOnChange(change)
             if '�' in change['title']: # Invalid title
                 logging.warning(f'replacement char found in title "{change.get('title')}" on change {change.get('revision')}')
